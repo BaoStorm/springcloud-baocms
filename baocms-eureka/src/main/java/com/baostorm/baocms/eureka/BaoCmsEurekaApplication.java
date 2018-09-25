489c9c0d80c2e780701b4352485e695ba5439b01
BaoStorm <609311029@qq.com>
2018-09-25 18:43:33 +0800

    +

---
+++ baocms-eureka/src/main/java/com/baostorm/baocms/eureka/BaoCmsEurekaApplication.java
@@ -0,0 +1,16 @@
+package com.baostorm.baocms.eureka;
+
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@EnableEurekaServer
+@SpringBootApplication
+public class BaoCmsEurekaApplication  {
+
+
+    public static void main(String[] args) {
+        SpringApplication.run(BaoCmsEurekaApplication.class, args);
+    }
+}
